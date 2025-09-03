5f3167ebe3b437049fe319312b5618a8dbb691e5
Yasemin Özhan (BilgeAdam Boost) <yasemin.ozhan@bilgeadamboost.com>
2025-09-03 19:35:38 +0300

    Add project files.

---
+++ ZerogApp.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZerogApp.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
