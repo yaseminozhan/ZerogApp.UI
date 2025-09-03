3337ce45837427eabb207335eba0448bb68e4094
Yasemin Özhan (BilgeAdam Boost) <yasemin.ozhan@bilgeadamboost.com>
2025-09-03 23:32:17 +0300

    .

--- ZerogApp.UI/Program.cs
+++ ZerogApp.UI/Program.cs
@@ -1,23 +1,53 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+// 1) Resource klasörünü tanýmla
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+// 2) View + DataAnnotations lokalizasyonunu aç
+builder.Services.AddControllersWithViews()
+    .AddViewLocalization(LanguageViewLocationExpanderFormat.SubFolder)
+    .AddDataAnnotationsLocalization();
+
+// 3) Desteklenen kültürler
+var supportedCultures = new[]
+{
+    new CultureInfo("tr"),
+    new CultureInfo("en"),
+    new CultureInfo("de"),
+    new CultureInfo("sk"),
+    new CultureInfo("fr")
+};
+
+var locOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("tr"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+};
+
+// Kullanýcý seçimini hatýrlamak için: cookie provider en baþta
+locOptions.RequestCultureProviders.Insert(0, new CookieRequestCultureProvider());
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// PIPELINE
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// 4) Lokalizasyon middleware (erken ekle)
+app.UseRequestLocalization(locOptions);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
